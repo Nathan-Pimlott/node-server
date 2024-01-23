5d8afb9b062c70096db871ffd689c65be4bd8586
Nathan Pimlott <nathanpimlott@Nathans-MacBook-Pro.local>
2024-01-23 21:40:36 +0000

    Added basic auth middleware and openapi spec

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Express } from "express";
 import { areNumbersSame } from "../utils/math";
+import { authMiddleware } from "../utils/middleware";
 
 export const routes = (app: Express) => {
+  app.use("*", authMiddleware);
+
   app.post("/", (req, res) => {
     console.log({ body: req.body });
 
     const { num1, num2 } = req.body;
+
+    if (!num1 || !num2) {
+      return res.status(422).send("Please provide 2 numbers.");
+    }
 
     const areSame = areNumbersSame(num1, num2);
 
